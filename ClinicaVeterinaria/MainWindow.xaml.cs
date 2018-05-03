61dbbd732b11f7452effa3648f7770aa24942821
jason franco <jada183.jf@gmail.com>
2018-05-03 10:19:32 +0200

    ventana de producto  basica

--- ClinicaVeterinaria/MainWindow.xaml.cs
+++ ClinicaVeterinaria/MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace ClinicaVeterinaria
     /// </summary>
     public partial class MainWindow : Window
     {
+        class Producto
+        {
+            string nombre;
+            double precio;
+            string marca;
+
+            public string Nombre { get => nombre; set => nombre = value; }
+            public double Precio { get => precio; set => precio = value; }
+            public string Marca { get => marca; set => marca = value; }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            List<Producto> listp = new List<Producto>();
+            for(int i =0; i < 10; i++)
+            {
+                Producto p = new Producto();
+                if(i % 2 ==0)
+                {
+                    p.Nombre = "Correa extensible";
+                    p.Precio = 5.20;
+                    p.Marca = "wiskas";
+                }
+                else
+                {
+                    p.Nombre = "Pienso";
+                    p.Precio = 8.20;
+                    p.Marca = "Purina";
+                }
+                listp.Add(p);
+            }
+            dgProd.ItemsSource = listp;
         }
+
+    
     }
 }
