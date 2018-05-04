340f061fec05519f76a3b0d14c57e44207b821d6
jason franco <jada183.jf@gmail.com>
2018-05-04 09:28:16 +0200

    agrego  migracion y tabla de proveedor y el campo de paciente "ingresado"

---
+++ ClinicaVeterinaria/MODEL/Proveedor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaVeterinaria.MODEL
+{
+    [Table("Proveedor")]
+    public class Proveedor: PropertyValidateModel
+    {
+        public Proveedor() {
+            Productos = new HashSet<Producto>();
+        }
+        public int ProveedorId { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Movil { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<Producto> Productos { get; set; }
+
+}
+}
