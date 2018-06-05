8b87a103ff1f9ece72a565cffc406ad6fd870e88
jason <jada183.jf@gmail.com>
2018-06-05 20:12:03 +0200

    cambio para poder modificar los campos por defecto de las tablas

--- ClinicaVeterinaria/Migrations/201806051810236_initial.Designer.cs
+++ ClinicaVeterinaria/Migrations/201806051810236_initial.Designer.cs
@@ -13,7 +13,7 @@ namespace ClinicaVeterinaria.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201806051704270_initial"; }
+            get { return "201806051810236_initial"; }
         }
         
         string IMigrationMetadata.Source
