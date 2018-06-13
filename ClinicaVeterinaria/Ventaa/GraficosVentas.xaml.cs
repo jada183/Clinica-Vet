d5231b7ad4791be782866e08e694d41e71514475
jason <jada183.jf@gmail.com>
2018-06-13 20:03:34 +0200

    Ajustes graficos

--- ClinicaVeterinaria/Ventaa/GraficosVentas.xaml.cs
+++ ClinicaVeterinaria/Ventaa/GraficosVentas.xaml.cs
@@ -33,25 +33,15 @@ namespace ClinicaVeterinaria.Ventaa
         }
        public void CargarGraficoMayoresVent()
         {
-            int primer = 0;
-            int segund = 0;
-            int tercero = 0;
-            int cuarto = 0;
-            int quinto = 0;
-            Producto primerPr = new Producto();
-            Producto segundPr = new Producto();
-            Producto terceroPr = new Producto();
-            Producto cuartoPr = new Producto();
-            Producto quintoPr = new Producto();
             List<Producto> productos = MainWindow.uow.RepositorioProducto.obtenerVarios(c => c.Habilitado == true);
-            List<Producto> productoOrdenados = productos.OrderBy(c => c.LineasVentas.Count).ToList();
+            List<Producto> productoOrdenados = productos.OrderByDescending(c => c.LineasVentas.Count).ToList();
             List<Producto> MasVendidos = new List<Producto>();
             for(int i = 0; i < 5; i++)
             {
                 MasVendidos.Add(productoOrdenados[i]);
             }
             // Create the plot model
-            var tmp = new PlotModel { Title = "Bar series", LegendPlacement = LegendPlacement.Outside, LegendPosition = LegendPosition.RightTop, LegendOrientation = LegendOrientation.Vertical };
+            var tmp = new PlotModel { Title = "Ventas en las que se compro el producto", LegendPlacement = LegendPlacement.Outside, LegendPosition = LegendPosition.RightTop, LegendOrientation = LegendOrientation.Vertical };
 
             // Add the axes, note that MinimumPadding and AbsoluteMinimum should be set on the value axis.
             tmp.Axes.Add(new CategoryAxis { Position = AxisPosition.Left, ItemsSource = MasVendidos, LabelField = "NombreProducto" });
