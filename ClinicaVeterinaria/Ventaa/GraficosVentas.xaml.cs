9c9460832c8f3fae26d0247cc5fd7846f0fdb24b
jason franco <jada183.jf@gmail.com>
2018-06-13 16:57:58 +0200

    ajuste ranking chart

--- ClinicaVeterinaria/Ventaa/GraficosVentas.xaml.cs
+++ ClinicaVeterinaria/Ventaa/GraficosVentas.xaml.cs
@@ -33,17 +33,32 @@ namespace ClinicaVeterinaria.Ventaa
         }
        public void CargarGraficoMayoresVent()
         {
+            int primer = 0;
+            int segund = 0;
+            int tercero = 0;
+            int cuarto = 0;
+            int quinto = 0;
+            Producto primerPr = new Producto();
+            Producto segundPr = new Producto();
+            Producto terceroPr = new Producto();
+            Producto cuartoPr = new Producto();
+            Producto quintoPr = new Producto();
             List<Producto> productos = MainWindow.uow.RepositorioProducto.obtenerVarios(c => c.Habilitado == true);
-
+            List<Producto> productoOrdenados = productos.OrderBy(c => c.LineasVentas.Count).ToList();
+            List<Producto> MasVendidos = new List<Producto>();
+            for(int i = 0; i < 5; i++)
+            {
+                MasVendidos.Add(productoOrdenados[i]);
+            }
             // Create the plot model
             var tmp = new PlotModel { Title = "Bar series", LegendPlacement = LegendPlacement.Outside, LegendPosition = LegendPosition.RightTop, LegendOrientation = LegendOrientation.Vertical };
 
             // Add the axes, note that MinimumPadding and AbsoluteMinimum should be set on the value axis.
-            tmp.Axes.Add(new CategoryAxis { Position = AxisPosition.Left, ItemsSource = productos, LabelField = "NombreProducto" });
+            tmp.Axes.Add(new CategoryAxis { Position = AxisPosition.Left, ItemsSource = MasVendidos, LabelField = "NombreProducto" });
             tmp.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, MinimumPadding = 0, AbsoluteMinimum = 0 });
 
             // Add the series, note that the BarSeries are using the same ItemsSource as the CategoryAxis.
-            tmp.Series.Add(new BarSeries { Title = "Ventas", ItemsSource = productos, ValueField = "LineasVentas.Count" });
+            tmp.Series.Add(new BarSeries { Title = "Ventas", ItemsSource = MasVendidos, ValueField = "LineasVentas.Count" });
 
             this.GraficoMayoresVentas = tmp;
 
