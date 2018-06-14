ebbce04d749651194d6c930245b9b86d4631a49d
jason franco <jada183.jf@gmail.com>
2018-06-14 14:49:07 +0200

    segundo boton genera excel pero tengo problemas con el formato de la fecha

--- ClinicaVeterinaria/Ventaa/GenerarExcels.xaml.cs
+++ ClinicaVeterinaria/Ventaa/GenerarExcels.xaml.cs
@@ -61,9 +61,9 @@ namespace ClinicaVeterinaria.Ventaa
                         {
                             excel.Workbook.Worksheets.Add("Ventas");
                             List<string[]> headerRow = new List<string[]>()
-                    {
-                       new string[] { "Nombre de cliente", "Apellidos de cliente", "Telefono de cliente","Email de cliente","Fecha  de la venta" }
-                    };
+                            {
+                               new string[] { "Nombre de cliente", "Apellidos de cliente", "Telefono de cliente","Email de cliente","Fecha  de la venta" }
+                            };
 
                             string headerRange = "A1:" + Char.ConvertFromUtf32(headerRow[0].Length + 64) + "1";
                             var worksheet = excel.Workbook.Worksheets["Ventas"];
@@ -92,7 +92,7 @@ namespace ClinicaVeterinaria.Ventaa
                                             worksheet.Cells[contforeach, i].LoadFromText(Convert.ToString(v.ClienteVenta.Email));
                                             break;
                                         case 5:
-                                            worksheet.Cells[contforeach, i].LoadFromText(Convert.ToString(v.FechaVenta));
+                                            worksheet.Cells[contforeach, i].LoadFromText(Convert.ToString(v.FechaVenta.Date));
                                             break;
                                     }
 
@@ -125,6 +125,95 @@ namespace ClinicaVeterinaria.Ventaa
                 MessageBox.Show(" no se ha encontrado ninguna venta para generar excel ");
             }
            
+        }
+
+        private void BtAceptarExcelProd_Click(object sender, RoutedEventArgs e)
+        {
+            int contforeach = 2;//para indicar la fila en la que se agregan los datos
+            Producto p = MainWindow.uow.RepositorioProducto.obtenerUno(c => c.Habilitado == true && tbMarcaProd.Text == c.NombreMarca && c.NombreProducto == tbNombreProd.Text);
+            if (p !=null)
+            {
+                List<LineaVenta> lv = MainWindow.uow.RepositorioLineaVenta.obtenerVarios(c => c.ProductoId == p.ProductoId);
+                if (lv.Count > 0)
+                {
+                    string messageBoxText = "Estas seguro que deseas generar excel? prodria tener algun excel generado para ese producto que se sobreescribiria";
+                    string caption = "Word Processor";
+                    MessageBoxButton button = MessageBoxButton.YesNoCancel;
+                    MessageBoxImage icon = MessageBoxImage.Warning;
+                    MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
+                    switch (result)
+                    {
+                        case MessageBoxResult.Yes:
+                            using (ExcelPackage excel = new ExcelPackage())
+                            {
+                                excel.Workbook.Worksheets.Add("Ventas");
+                                List<string[]> headerRow = new List<string[]>()
+                                {
+                                   new string[] {"Cantidad", "Fecha  de la venta", "Nombre de cliente", "Apellidos de cliente", "Telefono de cliente","Email de cliente","Usuario del empleado" }
+                                };
+                                string headerRange = "A1:" + Char.ConvertFromUtf32(headerRow[0].Length + 64) + "1";
+                                var worksheet = excel.Workbook.Worksheets["Ventas"];
+                                worksheet.Cells[headerRange].Style.Font.Bold = true;
+                                worksheet.Cells[headerRange].Style.Font.Size = 14;
+                                worksheet.DefaultColWidth = 25;
+                                worksheet.Cells[headerRange].Style.Font.Color.SetColor(System.Drawing.Color.Purple);
+                                foreach(LineaVenta l in lv)
+                                {
+                                    for(int i=1; i < 8; i++)
+                                    {
+                                        switch (i)
+                                        {
+
+                                            case 1:
+                                                worksheet.Cells[contforeach, i].LoadFromText(Convert.ToString(l.Cantidad));
+                                                break;
+                                            case 2:
+                                                worksheet.Cells[contforeach, i].LoadFromText(Convert.ToString((l.Venta.FechaVenta)));
+                                                break;
+                                            case 3:
+                                                worksheet.Cells[contforeach, i].LoadFromText(l.Venta.ClienteVenta.Nombre);
+                                                break;
+                                            case 4:
+                                                worksheet.Cells[contforeach, i].LoadFromText(l.Venta.ClienteVenta.Apellidos);
+                                                break;
+                                            case 5:
+                                                worksheet.Cells[contforeach, i].LoadFromText(l.Venta.ClienteVenta.Telefono);
+                                                break;
+                                            case 6:
+                                                worksheet.Cells[contforeach, i].LoadFromText(l.Venta.ClienteVenta.Email);
+                                                break;
+                                            case 7:
+                                                worksheet.Cells[contforeach, i].LoadFromText(l.Venta.EmpleadoVenta.Usuario);
+                                                break;
+                                        }
+                                    }
+                                    contforeach++;
+                                }
+                                worksheet.Cells[headerRange].LoadFromArrays(headerRow);
+                                try
+                                {
+                                    FileInfo excelFile = new FileInfo(Environment.CurrentDirectory + "\\excels\\" + p.NombreProducto+"-"+p.NombreMarca + ".xlsx");
+                                    excel.SaveAs(excelFile);
+                                    MessageBox.Show("se ha guardado el excel correctamente");
+                                    gridDatosProductos.Visibility = Visibility.Collapsed;
+                                }
+                                catch { }
+                            }
+
+                            break;
+                        case MessageBoxResult.No:
+
+                            break;
+                        case MessageBoxResult.Cancel:
+                            // User pressed Cancel button
+                            // ...
+                            break;
+                    }
+                }
+            }
+            else{
+                MessageBox.Show("no se ha encontrado ningun producto con esas especificaciones");
+            }
         }
     }
 }
